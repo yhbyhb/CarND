5a47fffc9a26ab45b804b4a2084aa194cb47da4e
HanByul Yang <hanbyul.yang@gmail.com>
2017-07-22 14:55:10 +0900

    Added class materials. Intro to neural networks

---
+++ Term1/class_materials/intro_to_neural_networks/16_gradient_descent_the_code.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def sigmoid(x):
+    """
+    Calculate sigmoid
+    """
+    return 1/(1+np.exp(-x))
+
+def sigmoid_prime(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+learnrate = 0.5
+x = np.array([1, 2])
+y = np.array(0.5)
+
+# Initial weights
+w = np.array([0.5, -0.5])
+
+# Calculate one gradient descent step for each weight
+# TODO: Calculate output of neural network
+nn_output = sigmoid(np.dot(w, x))
+
+# TODO: Calculate error of neural network
+error = y - nn_output
+error_term = error * sigmoid_prime(np.dot(w, x))
+
+# TODO: Calculate change in weights
+del_w = learnrate * error_term * x
+
+print('Neural Network output:')
+print(nn_output)
+print('Amount of Error:')
+print(error)
+print('Change in Weights:')
+print(del_w)
