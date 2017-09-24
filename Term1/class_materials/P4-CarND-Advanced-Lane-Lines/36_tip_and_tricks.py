4f87068fa7f8a23919d8c9e218e0b6fd4480b9aa
HanByul Yang <hanbyul.yang@gmail.com>
2017-09-24 15:27:26 +0900

    Added P4 project class materials. gitignore *.p

---
+++ Term1/class_materials/P4-CarND-Advanced-Lane-Lines/36_tip_and_tricks.py
@@ -0,0 +1,42 @@
+# Define a class to receive the characteristics of each line detection
+class Line():
+    def __init__(self):
+        # was the line detected in the last iteration?
+        self.detected = False  
+        # x values of the last n fits of the line
+        self.recent_xfitted = [] 
+        #average x values of the fitted line over the last n iterations
+        self.bestx = None     
+        #polynomial coefficients averaged over the last n iterations
+        self.best_fit = None  
+        #polynomial coefficients for the most recent fit
+        self.current_fit = [np.array([False])]  
+        #radius of curvature of the line in some units
+        self.radius_of_curvature = None 
+        #distance in meters of vehicle center from the line
+        self.line_base_pos = None 
+        #difference in fit coefficients between last and new fits
+        self.diffs = np.array([0,0,0], dtype='float') 
+        #x values for detected line pixels
+        self.allx = None  
+        #y values for detected line pixels
+        self.ally = None
+
+
+# Create an image to draw the lines on
+warp_zero = np.zeros_like(warped).astype(np.uint8)
+color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+# Recast the x and y points into usable format for cv2.fillPoly()
+pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+pts = np.hstack((pts_left, pts_right))
+
+# Draw the lane onto the warped blank image
+cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+# Warp the blank back to original image space using inverse perspective matrix (Minv)
+newwarp = cv2.warpPerspective(color_warp, Minv, (image.shape[1], image.shape[0])) 
+# Combine the result with the original image
+result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+plt.imshow(result)
---
+++ Term1/class_materials/P4-CarND-Advanced-Lane-Lines/code_snippet.py
@@ -0,0 +1,88 @@
+gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+
+sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+
+sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+
+abs_sobelx = np.absolute(sobelx)
+
+scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+
+
+thresh_min = 20
+thresh_max = 100
+sxbinary = np.zeros_like(scaled_sobel)
+sxbinary[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+plt.imshow(sxbinary, cmap='gray')
+
+
+def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
+    # Calculate directional gradient
+    # Apply threshold
+    return grad_binary
+
+def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
+    # Calculate gradient magnitude
+    # Apply threshold
+    return mag_binary
+
+def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):
+    # Calculate gradient direction
+    # Apply threshold
+    return dir_binary
+
+# Choose a Sobel kernel size
+ksize = 3 # Choose a larger odd number to smooth gradient measurements
+
+# Apply each of the thresholding functions
+gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(0, 255))
+grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(0, 255))
+mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(0, 255))
+dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0, np.pi/2))
+
+combined = np.zeros_like(dir_binary)
+combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+
+
+# Convert to HLS color space and separate the S channel
+# Note: img is the undistorted image
+hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+s_channel = hls[:,:,2]
+
+# Grayscale image
+# NOTE: we already saw that standard grayscaling lost color information for the lane lines
+# Explore gradients in other colors spaces / color channels to see what might work better
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+# Sobel x
+sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0) # Take the derivative in x
+abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
+scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+
+# Threshold x gradient
+thresh_min = 20
+thresh_max = 100
+sxbinary = np.zeros_like(scaled_sobel)
+sxbinary[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+
+# Threshold color channel
+s_thresh_min = 170
+s_thresh_max = 255
+s_binary = np.zeros_like(s_channel)
+s_binary[(s_channel >= s_thresh_min) & (s_channel <= s_thresh_max)] = 1
+
+# Stack each channel to view their individual contributions in green and blue respectively
+# This returns a stack of the two binary images, whose components you can see as different colors
+color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary))
+
+# Combine the two binary thresholds
+combined_binary = np.zeros_like(sxbinary)
+combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
+
+# Plotting thresholded images
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
+ax1.set_title('Stacked thresholds')
+ax1.imshow(color_binary)
+
+ax2.set_title('Combined S channel and gradient thresholds')
+ax2.imshow(combined_binary, cmap='gray')
