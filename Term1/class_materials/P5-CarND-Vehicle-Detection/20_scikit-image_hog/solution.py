d99151f62e0b6ae910a9c258edb0f03f4539da4b
HanByul Yang <hanbyul.yang@gmail.com>
2017-10-01 17:40:21 +0900

    Added class materials for P5 vehicle detection and tracking

---
+++ Term1/class_materials/P5-CarND-Vehicle-Detection/20_scikit-image_hog/solution.py
@@ -0,0 +1,12 @@
+# Define a function to return HOG features and visualization
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True):
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=False, 
+                                  visualise=True, feature_vector=False)
+        return features, hog_image
+    else:      
+        features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=False, 
+                       visualise=False, feature_vector=feature_vec)
+        return features
