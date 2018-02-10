bed19d2843e5830af77ef27f5cdd3f4ce7457781
HanByul Yang <hanbyul.yang@gmail.com>
2018-02-10 22:27:04 +0900

    added term3 lesson2 11. Quiz: Expansion Grid

---
+++ Term3/lesson02_search/11_quiz_expansion_grid.py
@@ -0,0 +1,76 @@
+# -----------
+# User Instructions:
+# 
+# Modify the function search so that it returns
+# a table of values called expand. This table
+# will keep track of which step each node was
+# expanded.
+#
+# Make sure that the initial cell in the grid 
+# you return has the value 0.
+# ----------
+
+grid = [[0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0]]
+init = [0, 0]
+goal = [len(grid)-1, len(grid[0])-1]
+cost = 1
+
+delta = [[-1, 0], # go up
+         [ 0,-1], # go left
+         [ 1, 0], # go down
+         [ 0, 1]] # go right
+
+delta_name = ['^', '<', 'v', '>']
+
+def search(grid,init,goal,cost):
+    # ----------------------------------------
+    # modify code below
+    # ----------------------------------------
+    closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+    closed[init[0]][init[1]] = 1
+
+    x = init[0]
+    y = init[1]
+    g = 0
+
+    open_list = [[g, x, y]]
+
+    found = False  # flag that is set when search is complete
+    resign = False # flag set if we can't find expand
+
+    expand = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
+    expand_count = 0
+
+    while not found and not resign:
+        if len(open_list) == 0:
+            resign = True
+        else:
+            open_list.sort()
+            open_list.reverse()
+            next_item = open_list.pop()
+
+            x = next_item[1]
+            y = next_item[2]
+            g = next_item[0]
+
+            expand[x][y] = expand_count
+            expand_count += 1
+            
+            if x == goal[0] and y == goal[1]:
+                found = True
+            else:
+                for i in range(len(delta)):
+                    x2 = x + delta[i][0]
+                    y2 = y + delta[i][1]
+                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
+                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                            g2 = g + cost
+                            open_list.append([g2, x2, y2])
+                            closed[x2][y2] = 1
+    return expand
+
+print(search(grid,init,goal,cost))
